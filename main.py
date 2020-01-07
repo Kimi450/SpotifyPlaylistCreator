e94b539c801d9ca663fcd20e1f9e6ae3e40f5f32
Karan Samani <karan_samani@yahoo.co.in>
2020-01-07 16:57:47 +0000

    Minor changes
    Added some error checking and functions to get artist and
    album data from a retrieved track.
    
    Levenstein distance used to select which track to select for a given
    file.
    
    Still need to work on the following:
    
    Better searches (maybe check for track name and artists separately).
    The 502 error sometimes comes back, still dont know why.

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import spotipy.util as util
 from os import listdir
 from os.path import isfile, join
 
-def get_ext_files(path,extensions, verbose = False, include_extension = True):
+from Levenshtein import distance
+
+def get_files(path,extensions, verbose = False, include_extension = True):
     if verbose:
         print(f"Retrieving files from path and extensions:\n{path}\n{extensions}...")
     files = []
@@ -31,6 +33,28 @@ def get_ext_files(path,extensions, verbose = False, include_extension = True):
         print(f"...{len(files)} files found")
     return files
 
+def get_token(client_id, client_secret, user, scope, redirect_uri = "http://localhost/"):
+    # using the spotipy library to perform user level auth
+    url = "https://accounts.spotify.com/authorize?"
+    token = util.prompt_for_user_token(user,scope, client_id = client_id,
+                                       client_secret = client_secret,
+                                       redirect_uri = redirect_uri)
+    return token
+
+def get_spotify_album_data(item):
+    album_artists = []
+    for object in item["album"]["artists"]:
+        album_artists.append(object["name"])
+    album_name = item["album"]["name"]
+    album_release_date = item["album"]["release_date"]
+    return (album_name, album_release_date, album_artists)
+
+def get_spotify_artist_data(item):
+    artists = []
+    for object in item["artists"]:
+        artists.append(object["name"])
+    return artists
+
 def create_playlist(user, name, description, token, public = False):
 
     endpoint_url = f"https://api.spotify.com/v1/users/{user}/playlists"
@@ -46,37 +70,9 @@ def create_playlist(user, name, description, token, public = False):
                                      "Authorization":f"Bearer {token}"})
     # print(response.json())
     # url = response.json()['external_urls']['spotify']
-    print(response.status_code)
     if response.status_code != 201:
         raise APIError(f"Couldn't create playlist \"{name}\" for \"{user}\"")
     return response
-
-def get_token_old(client_id, client_secret):
-    # limited usage, hence not using
-
-    # get token using the Client Credentials Flow
-    # https://developer.spotify.com/documentation/general/guides/authorization-guide/
-
-    # "client_ID:client_sec" encoded in base64 as per requirements
-    basic = bytes(client_id+':'+client_secret,"utf-8")
-    basic = base64.b64encode(basic).decode("utf-8")
-
-    response = requests.post(url = "https://accounts.spotify.com/api/token",
-                            data = {"grant_type" : "client_credentials"},
-                            headers={"Content-Type":"application/x-www-form-urlencoded",
-                                     "Authorization":f"Basic {basic}"})
-    return response.json()["access_token"]
-
-def get_token(client_id, client_secret, user, scope, redirect_uri = "http://localhost/"):
-    # using the spotipy library to perform user level auth
-    url = "https://accounts.spotify.com/authorize?"
-    token = util.prompt_for_user_token(user,scope, client_id = client_id,
-                                       client_secret = client_secret,
-                                       redirect_uri = redirect_uri)
-    return token
-
-class APIError(Exception):
-    pass
 
 def add_to_playlist(playlist_id, uris, token):
 
@@ -92,6 +88,58 @@ def add_to_playlist(playlist_id, uris, token):
         raise APIError(f"Couldn't add uris \"{uris}\" to playlist_id \"{playlist_id}\"")
     return True
 
+def get_best_match_uri(items, file):
+
+    best_match_cost = sys.maxsize
+    best_match_uri = None
+    for item_index, item in enumerate(items):
+        name = item["name"]
+        edit_dist_name = distance(file,name)
+        # print(f"{edit_dist_name:>2}: {file} -> {name}")
+        if edit_dist_name <= best_match_cost:
+            # print(get_spotify_album_data(item))
+            # print(get_spotify_artist_data(item))
+            best_match_cost = edit_dist_name
+            uri = item["uri"]
+            best_match_uri = uri
+
+    return best_match_uri
+
+def add_files_to_playlist(files, playlist_id, token, type = "track"):
+    unknown_files = []
+    endpoint_url = "https://api.spotify.com/v1/search?"
+    limit = 10
+    for count, file in enumerate(files):
+        file = "Cold Water (feat. Justin Bieber"
+        # file = "Keep Me In Your Heart For A While"
+        # file = "creep postmodern"
+        print(f"{count+1:>5} --- File ", end="")
+        query = f"{endpoint_url}q={file}&type={type}&limit={limit}"
+
+        response = requests.get(query,
+                                headers={"Content-Type":"application/json",
+                                         "Authorization":f"Bearer {token}"})
+        json_response = response.json()
+
+        if response.status_code != 200:
+            raise APIError(f"Failed query: {json_response}")
+
+        best_match_uri = get_best_match_uri(json_response['tracks']["items"], file)
+
+        if best_match_uri == None:
+            print(f"NOT found on Spotify: \"{file}\"")
+            unknown_files.append(file)
+            continue
+
+        add_to_playlist(playlist_id, [best_match_uri], token)
+        print(f"Added on spotify    : \"{file}\"")
+        # exit()
+
+        # parsed = json.loads(json.dumps(json_response))
+        # print(json.dumps(parsed, indent=4, sort_keys=True))
+
+    return unknown_files
+
 def main():
     if len(sys.argv) == 2:
         path = sys.argv[1]
@@ -101,47 +149,27 @@ def main():
 
     TOKEN = get_token(CLIENT_ID, CLIENT_SECRET,USER,"playlist-modify-private")
 
-    playlist_id = "5z4DQFUNLKmiN7au7dK781"
+    playlist_id = "1FHMnsDFjDrNdqpRr9l4lx"
 
     # uncomment the below 3 lines if you want to create a new folder
 
-    # response = create_playlist(USER, "Old", "Music I had before I migrated to Spotify.", TOKEN)
+    # response = create_playlist(USER, "Old",
+    #                            "Music I had before I migrated to Spotify.",
+    #                            TOKEN)
     # playlist_id = response.json()['id']
+
     # print(playlist_id)
 
-    endpoint_url = "https://api.spotify.com/v1/search?"
-    type = "track"
-    limit = 10
-    files = get_ext_files(path,["mp3", "m4a", "wav", "flac"],
-                          include_extension=False)
-    unknown_files = []
-    for count, file in enumerate(files):
-        # file = "Cold Water (feat. Justin Bieber"
+    files = get_files(path, ["mp3", "m4a", "wav", "flac"],
+                      include_extension = False)
 
-        print(f"{count+1:>5} --- FILE ---> {file}")
-        query = f"{endpoint_url}q={file}&type={type}&limit={limit}"
+    # files.reverse()
+    unknown_files = add_files_to_playlist(files,playlist_id, TOKEN)
 
-        response = requests.get(query,
-                                headers={"Content-Type":"application/json",
-                                "Authorization":f"Bearer {TOKEN}"})
-        json_response = response.json()
-        # print(f"{file}\n{query}\n\n{json_response}\n\n")
-        uris = []
-        for item_index, item in enumerate(json_response['tracks']["items"]):
-            uri = item["uri"]
-            uris.append(uri)
-            # print(item    _index, uri, item["name"])
+    print("*** These were not found ***")
+    for i in unknown_files: print(i)
 
-        if len(uris) == 0:
-            print(f"*** Not found on Spotify: \"{file}\"")
-            unknown_files.append(file)
-            continue
 
-        add_to_playlist(playlist_id, [uris[0]], TOKEN)
 
-    print("These were not found:")
-    for i in unknown_files: print("\t",i)
-
-    return unknown_files
 if __name__ == "__main__":
     main()
